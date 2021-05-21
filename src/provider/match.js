682dc7f71d95dcbc93e80949d43268d676eacf3e
rongbao <rbao123@qq.com>
2021-05-21 17:02:44 +0800

    添加bilibili音乐 nondanee/UnblockNeteaseMusic#711

--- src/provider/match.js
+++ src/provider/match.js
@@ -15,7 +15,7 @@ const provider = {
 
 const match = (id, source, data) => {
 	let meta = {}
-	const candidate = (source || global.source || ['qq', 'kuwo', 'migu']).filter(name => name in provider)
+	const candidate = (source || global.source || ['qq', 'kuwo', 'migu','bilibili']).filter(name => name in provider)
 	return find(id, data)
 		.then(info => {
 			meta = info
@@ -35,7 +35,14 @@ const match = (id, source, data) => {
 
 const check = url => {
 	const song = {size: 0, br: null, url: null, md5: null}
-	return Promise.race([request('GET', url, {'range': 'bytes=0-8191'}), new Promise((_, reject) => setTimeout(() => reject(504), 5 * 1000))])
+	//return Promise.race([request('GET', url, {'range': 'bytes=0-8191'}), new Promise((_, reject) => setTimeout(() => reject(504), 5 * 1000))])
+	let header = {'range': 'bytes=0-8191'}
+	if (url.includes("bilivideo.com")){
+		header  = {'range': 'bytes=0-8191',
+			'referer':"https://www.bilibili.com/"
+		}
+	}
+	return Promise.race([request('GET', url, header), new Promise((_, reject) => setTimeout(() => reject(504), 5 * 1000))])
 		.then(response => {
 			if (!response.statusCode.toString().startsWith('2')) return Promise.reject()
 			if (url.includes('qq.com'))
